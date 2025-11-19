168e36d846c8c6c4e43c4c69f0a71982b73a8136
Phong <phongnt.22itb@vku.udn.vn>
2025-11-19 11:55:14 +0700

    generate email Backend

--- api_backend/server.js
+++ api_backend/server.js
@@ -1,26 +1,26 @@
 const express = require('express');
 const cors = require('cors');
 
-// 1. Import các file router (logic con)
 const getLeadsRouter = require('./routes/getLeads.js');
 const updateLeadsRouter = require('./routes/updateLeads.js');
+const generateEmailRouter = require('./routes/generateEmail.js');
 
-// 2. Khởi tạo App
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 const PORT = 3000;
 
-// 3. Điều phối (Rất quan trọng)
-// Khi có ai đó gọi /getNewLeads -> hãy đưa họ đến file getLeadsRouter
+// getNewLeads
 app.use('/getNewLeads', getLeadsRouter);
 
-// Khi có ai đó gọi /markLeadAsOld -> hãy đưa họ đến file updateLeadsRouter
+// updateOldStatus
 app.use('/markLeadAsOld', updateLeadsRouter);
 
+// generateEmails
+app.use('/generateEmail', generateEmailRouter);
 
-// 4. Khởi động máy chủ
+// Start server
 app.listen(PORT, () => {
   console.log(`Backend API (đã chia file) đang chạy trên cổng ${PORT}`);
   console.log('Các API có sẵn:');
